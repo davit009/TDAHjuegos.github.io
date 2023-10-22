294b430add053e8f0f7f29b7cfdbf7ebe9ffdfd9
davit009 <1220234@alumno.um.edu.mx>
2023-10-21 23:09:18 -0600

    opeaciones

---
+++ math_games/game_1/scriptsres.js
@@ -0,0 +1,100 @@
+const maxLives = 3;
+const numQuestions = 15;
+let lives = maxLives;
+let currentQuestion = 0;
+
+var num1, num2, respuesta, score;
+score = 0;
+txt_resta = document.getElementById("resta");
+op1 = document.getElementById("op1");
+op2 = document.getElementById("op2");
+op3 = document.getElementById("op3");
+txt_msj = document.getElementById("msj");
+txt_resultado = document.getElementById("resultado");
+scoreHtml = document.getElementById("bar-score");
+livesHtml = document.getElementById("bar-lives");
+
+function comenzar(){
+	txt_resultado.innerHTML = "?";
+	txt_msj.innerHTML = "";
+	scoreHtml.innerHTML = `${score}/${numQuestions}`;
+	livesHtml.innerHTML = `${lives}/${maxLives}`;
+
+	// genera la resta - Numeros aleatorios entre 0 y 9
+do {
+    num1 = Math.round(Math.random()*9);
+    num2 = Math.round(Math.random()*9);
+} while (num1 < num2);
+respuesta = num1 - num2;
+resta.innerHTML = num1 + " - " + num2 + " = ";
+
+indiceOpCorrecta = Math.round(Math.random()*2);
+console.log(indiceOpCorrecta);
+
+if(indiceOpCorrecta == 0){
+    op1.innerHTML = respuesta;
+    op2.innerHTML = respuesta + Math.round(Math.random()*3) + 1;
+    op3.innerHTML = respuesta + Math.round(Math.random()*3) + 1;
+}
+if(indiceOpCorrecta == 1){
+    op1.innerHTML = respuesta + Math.round(Math.random()*3) + 1;
+    op2.innerHTML = respuesta;
+    op3.innerHTML = respuesta + Math.round(Math.random()*3) + 1;
+}
+if(indiceOpCorrecta == 2){
+    op1.innerHTML = respuesta + Math.round(Math.random()*3) + 1;
+    op2.innerHTML = respuesta + Math.round(Math.random()*3) + 1;
+    op3.innerHTML = respuesta;
+}
+}
+
+function loseLive() {
+    lives--;
+    livesHtml.innerHTML = lives;
+	reducirBarraLives((lives/maxLives)*100);
+}
+
+function controlarRespuesta(opcionElegida){	
+	txt_resultado.innerHTML = opcionElegida.innerHTML;
+	if(respuesta == opcionElegida.innerHTML){
+		txt_msj.innerHTML = "¡Excelente!";
+		txt_msj.style.color="green";
+         // progress bar
+        if(currentQuestion < numQuestions) {
+			currentQuestion++;
+			score++;
+			avanzarBarraScore((score/numQuestions)*100);
+            setTimeout(comenzar, 2000);
+        }
+	}else{
+		txt_msj.innerHTML = "Intenta otra vez";
+		txt_msj.style.color="red";
+        if(lives > 0) {
+			loseLive();
+            setTimeout(limpiar, 2000);
+        }
+	}
+}
+
+function limpiar(){
+	txt_resultado.innerHTML = "?";
+	txt_msj.innerHTML = "";
+}
+
+comenzar();
+
+var scoreBar = document.getElementById('score_bar');
+scoreBar.style.width = `0%`;
+function avanzarBarraScore(porcentaje) {
+    scoreBar.style.transitionDuration = `0.5s`;
+    scoreBar.style.width =`${porcentaje}%`;
+	scoreHtml.innerHTML = `${score}/${numQuestions}`;
+}
+
+var livesBar = document.getElementById('lives_bar');
+livesBar.style.width = `100%`;
+function reducirBarraLives(porcentaje) {
+    livesBar.style.transitionDuration = `0.5s`;
+    livesBar.style.width =`${porcentaje}%`;
+	livesHtml.innerHTML = `${lives}/${maxLives}`;
+}

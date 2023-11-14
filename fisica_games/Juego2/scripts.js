d50b7b832fbc79e412bf1de8fbb9d46ee77f85cf
davit009 <1220234@alumno.um.edu.mx>
2023-11-14 00:13:19 -0600

    new

--- fisica_games/Juego2/scripts.js
+++ fisica_games/Juego2/scripts.js
@@ -1,59 +1,62 @@
 document.addEventListener("DOMContentLoaded", () => {
   let arrastrables = document.querySelectorAll(".arrastrable");
-  let contenedores = document.querySelectorAll(".contenedor");
+  let areaConductores = document.getElementById("areaConductores");
+  let areaAislantes = document.getElementById("areaAislantes");
+
+  // Contadores para los elementos colocados correctamente
+  let contadorConductores = 0;
+  let contadorAislantes = 0;
 
   arrastrables.forEach((arrastrable) => {
-    arrastrable.addEventListener("dragstart", (e) => {
-      e.dataTransfer.setData("text/plain", e.target.id);
-    });
+      arrastrable.addEventListener("dragstart", (e) => {
+          e.dataTransfer.setData("text/plain", e.target.id);
+      });
   });
 
-  contenedores.forEach((contenedor) => {
-    contenedor.addEventListener("dragover", (e) => {
-      e.preventDefault();
-    });
+  // Función para verificar si todos los elementos han sido colocados correctamente
+  function verificarColocacionCorrecta() {
+    const totalConductores = document.querySelectorAll(".arrastrable[data-tipo='conductor']").length;
+    const totalAislantes = document.querySelectorAll(".arrastrable[data-tipo='aislante']").length;
 
-    contenedor.addEventListener("drop", (e) => {
-      e.preventDefault();
-      const id = e.dataTransfer.getData("text/plain");
-      const arrastrable = document.getElementById(id);
-      const tipoObjeto = arrastrable.getAttribute("data-tipo");
+    if (contadorConductores === totalConductores && contadorAislantes === totalAislantes) {
+        // Mostrar la imagen de felicitaciones
+        document.getElementById("mensajeFelicitaciones").style.display = "block";
 
-      if (
-        (tipoObjeto === "conductor" && e.target.id === "areaConductores") ||
-        (tipoObjeto === "aislante" && e.target.id === "areaAislantes")
-      ) {
-        e.target.appendChild(arrastrable);
-      } else if (tipoObjeto === "conductor") {
-        console.log("El conductor no puede ir en el área de aislantes");
-        var advertencia = document.getElementById("advertencia");
-        advertencia.textContent =
-          "El conductor no puede ir en el área de aislantes"; // Cambia el mensaje
-        advertencia.style.display = "block";
+        setTimeout(() => {
+            window.location.href = "..\fisica.html"; // Cambia esto a la URL que prefieras
+        }, 3000);
+    }
+}
 
-        setTimeout(function () {
-          advertencia.style.display = "none";
-        }, 2000); // Ajusta este valor según tus necesidades
-      } else if (tipoObjeto === "aislante") {
-        console.log("El aislante no puede ir en el área de conductores");
-        var advertencia = document.getElementById("advertencia");
-        advertencia.textContent =
-          "El aislante no puede ir en el área de conductores"; // Cambia el mensaje
-        advertencia.style.display = "block";
+  [areaConductores, areaAislantes].forEach((contenedor) => {
+      contenedor.addEventListener("dragover", (e) => {
+          e.preventDefault();
+      });
 
-        setTimeout(function () {
-          advertencia.style.display = "none";
-        }, 2000); // Ajusta este valor según tus necesidades
-      } else {
-        console.log("Objeto no corresponde a esta área");
-        var advertenciag = document.getElementById("advertenciag");
-        advertenciag.textContent = "Objeto no corresponde a esta área"; // Mensaje general
-        advertenciag.style.display = "block";
+      contenedor.addEventListener("drop", (e) => {
+          e.preventDefault();
+          const id = e.dataTransfer.getData("text/plain");
+          const arrastrable = document.getElementById(id);
+          const tipoObjeto = arrastrable.getAttribute("data-tipo");
 
-        setTimeout(function () {
-          advertencia.style.display = "none";
-        }, 2000); // Ajusta este valor según tus necesidades
-      }
-    });
+          if (tipoObjeto === "conductor" && e.target.id === "areaConductores") {
+              e.target.appendChild(arrastrable);
+              contadorConductores++;
+          } else if (tipoObjeto === "aislante" && e.target.id === "areaAislantes") {
+              e.target.appendChild(arrastrable);
+              contadorAislantes++;
+          } else {
+              // Mostrar advertencias si es necesario
+              var advertencia = document.getElementById("advertencia");
+              advertencia.textContent = "Objeto no corresponde a esta área";
+              advertencia.style.display = "block";
+
+              setTimeout(function () {
+                  advertencia.style.display = "none";
+              }, 2000);
+          }
+
+          verificarColocacionCorrecta();
+      });
   });
 });
